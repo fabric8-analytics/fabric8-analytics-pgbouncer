9f1a5321f6a00dd4d33472a77e82aac456a7d6b0
Michal Srb <michal@redhat.com>
2017-11-09 17:32:40 +0100

    Revert "Update health-check-probe.sh"
    This reverts commit 612345c0215ade9e0f836aa78d1a4fd75d1ced0b.

--- health-check-probe.sh
+++ health-check-probe.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # This script is used as a liveness probe.
-echo "localhost:5432:$POSTGRESQL_INITIAL_DATABASE:$POSTGRESQL_USER:$POSTGRESQL_PASSWORD" > ~/.pgpass
-chmod 0600 ~/.pgpass
-psql -h localhost -U $POSTGRESQL_USER -d $POSTGRESQL_INITIAL_DATABASE -tc "SELECT 1;" | grep -q 1
+
+PGPASSWORD=$POSTGRESQL_PASSWORD psql -h localhost -U $POSTGRESQL_USER -d $POSTGRESQL_INITIAL_DATABASE -tc "SELECT 1;" | grep -q 1
