d1dd86812e14d5bebc5ee2e9b0cd67baa83a0aa4
Michal Srb <michal@redhat.com>
2017-11-09 17:32:34 +0100

    Revert "Add empty line at the end of health check probe"
    This reverts commit 8c34e97c4b8fe607922835bee70ff5220144aa5b.

--- health-check-probe.sh
+++ health-check-probe.sh
@@ -3,4 +3,3 @@
 echo "localhost:5432:$POSTGRESQL_INITIAL_DATABASE:$POSTGRESQL_USER:$POSTGRESQL_PASSWORD" > ~/.pgpass
 chmod 0600 ~/.pgpass
 psql -h localhost -U $POSTGRESQL_USER -d $POSTGRESQL_INITIAL_DATABASE -tc "SELECT 1;" | grep -q 1
-
